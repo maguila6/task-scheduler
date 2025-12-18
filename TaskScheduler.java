4bf31369be91005a6b8af7b60ea0cf2456959d9c
Maria Aguilar Felipe <maguilarfelipe79@gmail.com>
2025-12-18 15:17:06 -0500

    Refactor TaskScheduler to use a task map

--- TaskScheduler.java
+++ TaskScheduler.java
@@ -1,9 +1,25 @@
-private boolean higherPriority(Task a, Task b) {
-    if (a.getPriority() != b.getPriority())
-        return a.getPriority() > b.getPriority();
+import java.util.HashMap;
+import java.util.Map;
 
-    if (!a.getDeadline().equals(b.getDeadline()))
-        return a.getDeadline().isBefore(b.getDeadline());
+public class TaskScheduler {
+    private PriorityTaskQueue queue;
+    private Map<String, Task> taskMap;
 
-    return a.getDurationMinutes() < b.getDurationMinutes();
+    public TaskScheduler() {
+        queue = new PriorityTaskQueue();
+        taskMap = new HashMap<>();
+    }
+
+    public void addTask(Task task) {
+        queue.add(task);
+        taskMap.put(task.getId(), task);
+    }
+
+    public Task getNextTask() {
+        return queue.poll();
+    }
+
+    public boolean containsTask(String id) {
+        return taskMap.containsKey(id);
+    }
 }
